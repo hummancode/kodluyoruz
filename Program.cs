fc74a384333da033395fc42078d61842a5fcd4a1
HUMMAN <93881113+hummancode@users.noreply.github.com>
2022-06-16 09:01:33 +0300

    hata_yonetimi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System; 
+namespace hata_yonetimi {
+    class Program {
+        static void Main (string [] args) {
+            try {
+            Console.WriteLine("Sayı gir:"); 
+            int sayi  = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Sayı: "+sayi); 
+            }
+            catch(Exception ex) {
+                Console.WriteLine("Hata: "+ex.Message.ToString()); 
+
+            }
+            finally {
+                Console.WriteLine("İşlem tamam."); 
+
+            }
+       
+        }
+    }
+}
